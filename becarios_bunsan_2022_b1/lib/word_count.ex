cf88aa343776e603dc5dfe87c1d895f07223813a
Tania <taniiimedina@gmail.com>
2022-03-06 21:48:14 -0600

    WordCount

--- becarios_bunsan_2022_b1/lib/word_count.ex
+++ becarios_bunsan_2022_b1/lib/word_count.ex
@@ -1,5 +1,18 @@
 defmodule WordCount do 
-    def count do
-        "hola"
+    def count (path) do
+        {:ok, doc} = File.read(path)
+        doc = String.downcase(doc)
+        doc1 = String.split(doc, [",",";",":",".","_","-","|","°","#","$","%","&","/","(",")","()","{}","=","+","*",">","<","[","]","\r","?","¿", "!","¡"]) |> List.to_string()
+        doc2 = String.split(doc1)
+        mapa = Enum.map(doc2, fn x -> 
+            cont = Enum.count(doc2, fn s -> 
+                s == x
+            end)
+            Map.put(%{}, x, cont)
+        end)
+        Enum.reduce(mapa, %{}, fn (x, acc) -> 
+            Map.merge(acc, x) 
+        end)
     end
 end
+
