db3367e26e303e320d14d634bfae7a75a17f2da0
Tania <taniiimedina@gmail.com>
2022-03-07 23:56:08 -0600

    Pruebas Unitarias 2.0

--- becarios_bunsan_2022_b1/test/fizzbuz_test.exs
+++ becarios_bunsan_2022_b1/test/fizzbuz_test.exs
@@ -2,8 +2,17 @@ defmodule FizzBuzzTest do
     use ExUnit.Case
     doctest FizzBuzz
   
+    test "fizz" do
+      assert FizzBuzz.f(3) == IO.puts("Fizz")
+    end
+    test "buzz" do
+      assert FizzBuzz.f(10) == IO.puts("Buzz")
+    end
     test "fizzbuzz" do
-      assert FizzBuzz.call(15) == :ok
+      assert FizzBuzz.f(15) == IO.puts("FizzBuzz")
+    end
+    test "numero" do
+      assert FizzBuzz.f(7) == IO.puts(7)
     end
   end
   
