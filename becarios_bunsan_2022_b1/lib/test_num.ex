470788a110a5e4d607bb4f8d5d3d810187dfddd7
Tania <taniiimedina@gmail.com>
2022-03-21 21:03:33 -0600

    My Profile Card

---
+++ becarios_bunsan_2022_b1/lib/test_num.ex
@@ -0,0 +1,14 @@
+defmodule TestNum do
+
+    def test(x) when is_number(x) and x < 0  do
+      :negative
+    end
+  
+    def test(0) do
+      :zero
+    end
+  
+    def test(x) when is_number(x) and  x > 0 do
+      :positive
+    end
+  end
