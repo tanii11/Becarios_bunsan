bb81df732c81aa83dce4ac66cb4b15443e203c83
Tania <taniiimedina@gmail.com>
2022-05-04 21:51:35 -0500

    word_count

--- becarios_bunsan_2022_b1/lib/process_ring.ex
+++ becarios_bunsan_2022_b1/lib/process_ring.ex
@@ -1,14 +1,59 @@
 defmodule ProcessRing do
   def init(n) do
-    1..n |> Enum.map(fn x ->
-      spawn(ProcessRing, :message, [])
+    pids = Enum.map(1..n, fn _ -> spawn(&wait_for_config/0) end)
+    next_pids = Enum.drop(pids, 1) ++ [hd(pids)]
+    ring_config = Enum.zip(pids, next_pids)
+
+    IO.inspect(ring_config)
+
+    ring_config
+    |> hd()
+    |> then(fn {pid, next} -> send(pid, {:config, next, true}) end)
+
+    ring_config
+    |> Enum.drop(1)
+    |> Enum.each(fn {pid, next_pid} ->
+      send(pid, {:config, next_pid, false})
     end)
+
+    hd(pids)
   end
 
-  def message do
+  def rounds(pid, msg, n) do
+    send(pid, {msg, n})
+  end
+
+  # funcion para crear el proceso sin parametros
+  def wait_for_config() do
     receive do
-      {who, pid} -> send(pid, "Hello #{who} from #{inspect(self())}")
-      _ -> IO.puts("uh??")
+      {:config, next_pid, main} ->
+        IO.puts("pid: #{inspect(self())}, next: #{inspect(next_pid)} main: #{main}")
+        pid0 = if(main == true) do
+                 self()
+               end
+        process_msg(next_pid, main, pid0)
+      _ -> :ok
     end
+  end
+
+  def process_msg(next, main, pid0) do
+    receive do
+      {msg, n} ->
+        IO.puts("Process #{inspect(self())} received message \"#{msg}\", round #{n}")
+        cond do
+
+          main and n > 0 ->
+            IO.inspect(pid0)
+            send(next, {msg, n-1})
+          main -> :ok
+          true -> send(next, {msg, n})
+              # cond do
+              #   next == pid0 -> send(next, {msg, n-1})
+              #   true -> send(next, {msg, n})
+              # end
+        end
+      _ -> :ok
+    end
+    process_msg(next, main, pid0)
   end
 end
--- becarios_bunsan_2022_b1/lib/word_count.ex
+++ becarios_bunsan_2022_b1/lib/word_count.ex
@@ -1,18 +1,35 @@
-defmodule WordCount do 
-    def count (path) do
+defmodule WordCount do
+    def frequencies_tasks(path) do
+        mapas = path
+        |> File.stream!()
+        |> Enum.chunk_every(1_00)
+        |> Enum.map(fn lines -> Task.async(fn -> word_count(Enum.join(lines)) end) end)
+        |> Enum.map(fn task -> Task.await(task) end)
+
+        Enum.reduce(mapas, %{}, fn x, acc ->
+            Map.merge(x, acc, fn _k, v1, v2->
+               v1+v2
+            end)
+        end)
+    end
+
+    def frecuencies (path) do
         {:ok, doc} = File.read(path)
+        word_count(doc)
+    end
+
+    def word_count(doc) do
         doc = String.downcase(doc)
         doc1 = String.split(doc, [",",";",":",".","_","-","|","°","#","$","%","&","/","(",")","()","{}","=","+","*",">","<","[","]","\r","?","¿", "!","¡"]) |> List.to_string()
         doc2 = String.split(doc1)
-        mapa = Enum.map(doc2, fn x -> 
-            cont = Enum.count(doc2, fn s -> 
+        mapa = Enum.map(doc2, fn x ->
+            cont = Enum.count(doc2, fn s ->
                 s == x
             end)
             Map.put(%{}, x, cont)
         end)
-        Enum.reduce(mapa, %{}, fn (x, acc) -> 
-            Map.merge(acc, x) 
+        Enum.reduce(mapa, %{}, fn (x, acc) ->
+            Map.merge(acc, x)
         end)
     end
 end
-
