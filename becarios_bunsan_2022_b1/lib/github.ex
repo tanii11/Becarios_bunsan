687f9c5ca4b6e6661c8d766f678377f490c513de
Tania <taniiimedina@gmail.com>
2022-05-12 11:34:01 -0500

    rabbitmq tutorial

--- becarios_bunsan_2022_b1/lib/github.ex
+++ becarios_bunsan_2022_b1/lib/github.ex
@@ -14,20 +14,12 @@ defmodule GitHub do
   def repo_info(owner, repo) do
     client = new(1234)
     {:ok, languages} = languages(client, owner, repo)
-    list_languages = Map.keys(languages.body) |> Enum.reduce("", fn x, acc ->
-      if acc == "" do
-        x <> acc
-      else
-        acc <> ", " <> x
-      end
-    end)
     {:ok, commits} = commits(client, owner, repo)
-    num_commits = Enum.count(commits.body, fn x -> x end)
+    commits_num = Enum.count(commits.body, fn x -> x end)
     {:ok, name} = repo(client, owner, repo)
     name_repo = name.body["name"]
 
-    IO.puts("Name Repo: #{name_repo} , Number of commits: #{num_commits}, Languages: #{list_languages}")
-
+    #%{:name => name_repo, :commits_num => commits_num, :languages => languages.body}
   end
 
   defp new(token) do
