952849a4ce1ea17cd6e3c13b0e4e4242ae28da25
Tania <taniiimedina@gmail.com>
2022-05-13 16:21:34 -0500

    RabbitMQ baby steps 2

--- rabbitmq_tutorials/lib/rabbitmq_consumer.ex
+++ rabbitmq_tutorials/lib/rabbitmq_consumer.ex
@@ -6,24 +6,26 @@ defmodule RabbitMQ.Consumer do
     iex> {:ok, pid} = Consumer.start("orders")
   """
   use AMQP
+
   def start(queue_name) do
+
+    spawn(fn -> wait_for_messages(queue_name) end)
+  end
+
+  def wait_for_messages(queue_name) do
     #se crea la conexión
     {:ok, connection} = Connection.open()
     {:ok, channel} = Channel.open(connection)
+
     Basic.consume(channel, queue_name, nil, no_ack: true)
-    IO.puts " [*] Waiting for messages. To exit press CTRL+C, CTRL+C"
-    wait_for_messages(channel)
-
-    #spawn(fn -> wait_for_messages(channel) end)
-
-  end
-
-  def wait_for_messages(channel) do
     receive do
       {:basic_deliver, payload, meta} ->
         IO.puts " [x] Received [#{meta.routing_key}] #{payload} #{inspect(self())}"
+        Connection.close(connection)
+        wait_for_messages(queue_name)
 
-        wait_for_messages(channel)
+      :stop -> Connection.close(connection)
+        :ok
     end
   end
   @doc """
@@ -32,6 +34,6 @@ defmodule RabbitMQ.Consumer do
     iex> Consumer.stop("orders")
   """
   def stop(pid) do
-    AMQP.Connection.close(pid)
+    send(pid, :stop)
   end
 end
